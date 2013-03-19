74673295d57d625f839596df60904588e1c7c1be
Nagatoshi Matsushita <gurimusan@gmail.com>
2013-03-20 00:43:57 +0900

    change mx record

--- devproffice.info.lua
+++ devproffice.info.lua
@@ -5,10 +5,4 @@ a(_a, ipaddr, ttl)
 cname("*", _a, ttl)
 
 a(concat("mail", _a), ipaddr, ttl)
-a(concat("mailhost", _a), ipaddr, ttl)
-a(concat("lists", _a), ipaddr, ttl)
-
-mx(_a, concat("mailhost", _a), 10, ttl)
-mx(concat("lists", _a), concat("mailhost", _a), 10, ttl)
-txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
-txt(concat("lists", _a), "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
+mx(_a, concat("mail", _a), 10, ttl)
